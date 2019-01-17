6a7492e035e54c939fac0b8083d5e5e985e25d9e
Andrea Colleoni <andrea@colleoni.info>
2019-01-17 14:46:30 +0100

    sistemato conn string

--- LogicLayer/DBManager.cs
+++ LogicLayer/DBManager.cs
@@ -7,12 +7,14 @@ namespace LogicLayer
     internal class DBManager
     {
         private const string DB_CATALOG = "DellOrtoLuraschi";
+        private const string DB_USER = "dellorto";
+        private const string DB_PASSWORD = "passw0rd";
 
         internal static DbConnection getConnection()
         {
             string connectionString = String.Format(
             "Data Source=(local);Initial Catalog={0};"
-            + "Integrated Security=true", DB_CATALOG);
+            + "User Id={1};Password={2};", DB_CATALOG, DB_USER, DB_PASSWORD);
 
             return new SqlConnection(connectionString);
         }
